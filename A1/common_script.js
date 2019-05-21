1427b8904cf8a672619a9c450860cf7f1f32442f
yaoliu93 <io.yao.liu@gmail.com>
2019-05-21 02:50:32 -0700

    Add styling

--- A1/common_script.js
+++ A1/common_script.js
@@ -1,4 +1,4 @@
 document.getElementById("topnav").innerHTML =
-    '<a href="index.html">Home</a>' +
-    '<a href="about.html">About</a>' +
-    '<a href="dogs.html">Dogs</a>';
+    '<a href="index.html">HOME </a>' +
+    '<a href="about.html"> ABOUT </a>' +
+    '<a href="dogs.html"> DOGS</a>';
--- A1/index.js
+++ A1/index.js
@@ -1,4 +1,4 @@
-let messages_json = "[{\"message\":\"This is the first test message!\",\"timestamp\":\"Tue May 21 2019 00:10:52 GMT-0700 (Pacific Daylight Time)}\"},{\"message\":\"Would you look at that, here's another!\",\"timestamp\":\"Tue May 21 2019 03:15:52 GMT-0700 (Pacific Daylight Time)}\"},{\"message\":\"Three's a crowd!\",\"timestamp\":\"Tue May 21 2019 09:16:52 GMT-0700 (Pacific Daylight Time)}\"}]"
+let messages_json = "[{\"message\":\"This is the first test message!\",\"timestamp\":\"Tue May 21 2019 00:10:52 GMT-0700 (Pacific Daylight Time)\"},{\"message\":\"Would you look at that, here's another!\",\"timestamp\":\"Tue May 21 2019 03:15:52 GMT-0700 (Pacific Daylight Time)\"},{\"message\":\"Three's a crowd!\",\"timestamp\":\"Tue May 21 2019 09:16:52 GMT-0700 (Pacific Daylight Time)\"}]"
 let messages = JSON.parse(messages_json);
 let doc = document.getElementById("message-display-container");
 
@@ -14,8 +14,16 @@ clearMessages = () => {
     alert("What have you done!")
 }
 
+clearTextArea = () => {
+    document.getElementById("message-content").value = ""
+}
 document.getElementById("message-form").onsubmit = () => {
     let data = document.getElementById("message-content");
+    if (data.value === "") {
+        alert("I know you have something to say.")
+        return false
+    }
+
     let message = {
         "message": data.value,
         "timestamp": new Date + ""
@@ -27,10 +35,15 @@ document.getElementById("message-form").onsubmit = () => {
 };
 
 addMessage = (message) => {
-    let newMessage = document.createElement('div');
-    newMessage.id = messages.length;
-    newMessage.className = "message-box";
-    newMessage.innerHTML = message.message;
+    let timestamp_container = document.createElement('div');
+    timestamp_container.className = "timestamp-container";
+    timestamp_container.innerHTML = message.timestamp;
 
-    doc.appendChild(newMessage);
+    let newMessageContainer = document.createElement('div');
+    newMessageContainer.id = messages.length;
+    newMessageContainer.className = "message-container";
+    newMessageContainer.innerHTML = message.message;
+    newMessageContainer.appendChild(timestamp_container)
+
+    doc.appendChild(newMessageContainer);
 }

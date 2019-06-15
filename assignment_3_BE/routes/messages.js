543b426f809fbe77dadea325724b3c825a9783ca
yaoliu93 <io.yao.liu@gmail.com>
2019-06-15 14:26:29 -0700

    Update backend formatting

--- assignment_3_BE/routes/messages.js
+++ assignment_3_BE/routes/messages.js
@@ -2,46 +2,45 @@ var express = require('express');
 var router = express.Router();
 const uuidv4 = require('uuid/v4');
 
-/* GET list of messages. */
 let messages = [
   // you wouldn't store this in a list... you'd use a database
   {
-    message: 'This is one of those awesome messages.',
-    name: 'Jimmy',
+    message: 'FROM THE BACKEND: This is one of those awesome messages.',
+    name: 'BE Jimmy',
     timestamp: 'Tue May 21 2019 00:10:52 GMT-0700 (Pacific Daylight Time)',
     uuid: 'd2b33931-6e60-4ba0-89bb-31639df87774'
   },
   {
-    message: 'Oh look, another awesome message.',
-    name: 'Queen',
+    message: 'FROM THE BACKEND: Oh look, another awesome message.',
+    name: 'BE Queen',
     timestamp: 'Tue May 21 2019 00:10:52 GMT-0700 (Pacific Daylight Time)',
     uuid: '5c7ce07f-024b-47f0-8d11-997c9b64bf77'
   },
   {
-    message: 'Funny.',
-    name: 'Not',
+    message: 'FROM THE BACKEND: Funny.',
+    name: 'BE Not',
     timestamp: 'Tue May 21 2019 00:10:52 GMT-0700 (Pacific Daylight Time)',
     uuid: '86699da0-cdb6-48da-950c-79aba08542b2'
   }
 ];
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.json(messages);
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   req.body.uuid = uuidv4();
   messages.push(req.body);
   res.status(201).json(req.body);
 });
 
-router.delete('/', function(req, res, next) {
+router.delete('/', function (req, res, next) {
   messages = [];
   console.log('What have you done...You deleted everything!');
   res.status(204).send();
 });
 
-router.put('/:uuid', function(req, res, next) {
+router.put('/:uuid', function (req, res, next) {
   //params format: messages/d2b33931-6e60-4ba0-89bb-31639df87774
   let index = messages.findIndex(messages => messages.uuid === req.params.uuid);
   if (index !== -1) {
@@ -55,7 +54,7 @@ router.put('/:uuid', function(req, res, next) {
   }
 });
 
-router.delete('/:uuid', function(req, res, next) {
+router.delete('/:uuid', function (req, res, next) {
   //params format: messages/d2b33931-6e60-4ba0-89bb-31639df87774
   let index = messages.findIndex(messages => messages.uuid === req.params.uuid);
   if (index !== -1) {
